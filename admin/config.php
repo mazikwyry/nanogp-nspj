c863edbfb9674bb347b4994e1d365a1ebdae08db
mazikwyry <mazik.wyry@gmail.com>
2019-04-01 23:53:43 +0200

    init

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$cfg_client_id          = '609951808800-qf358cpuf2ja4mg4vmd54e4b3d6bddi2.apps.googleusercontent.com';
+$cfg_client_secret      = 'FWeJGpS62urznvz2VaYsxZmv';
+$cfg_max_accounts       = 1;
+$cfg_log                = false;
+$albums_filter          = ['sauvegarde', 'backup'];
+?>
